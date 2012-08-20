abc61accdd3500561fed14f48064ecb103929998
Joe Khoobyar <jkhoobyar@tnsi.com>
2012-08-20 03:18:54 -0400

    updated directory indexes

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -3,7 +3,7 @@ GIT=`which git 2>/dev/null`
 for DIR in $(find -type d -not -name .git -print -or -prune); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\." | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
+    ls -1pa "${DIR}" | grep -v "^\.[^\.]" | grep -v "^index\.html$" | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
   [ -z "$GIT" ] || "$GIT" add "${DIR}/index.html"
